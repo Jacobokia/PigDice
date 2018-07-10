33c601be0962913799f0c7a68c96937414d1aca7
Jacob Okia <cobs399@gmail.com>
2018-07-10 07:18:29 +0300

    prototypes for holdcheck and alerting the winner

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,42 @@ function Player(roll,currentScore,totalScore,turn, playerName) {
   this.currentScore = 0;
   this.totalScore = 0;
   this.turn = turn;
-  this.playerName = playerName;
+  this.playerName = "";
 }
 
-var Player1 = Math.floor(Math.random () * 6) + 1;
-var Player2 = Math.floor(Math.random() * 6) + 1;
+
+//creating  a prototype for checking a roll for a new object(rollCheck)
+Player.prototype.rollCheck = function () {
+  if (this.roll === 1){
+    this.currentScore = 0;
+    alert("sorry" + this.playerName + "" + "You lose your points")
+  }
+  else {
+    this.currentScore += this.roll;
+  }
+};
+
+//checking for hold the Dice
+Player.prototype.holdCheck = function(){
+  this.totalScore += currentScore;
+  this.currentScore = 0;
+  alert(this.playerName + "" + "sorry!your turn is over")
+}
+
+//checking for the winner
+Player.prototype.winnerCheck = function(){
+  if (this.totalscore >= 100){
+    alert(this.playerName + "" + "You are the winner")
+  }
+}
+
+//setting new game to  start 0
+Player.prototype.newGame = function(){
+  this.roll = 0;
+  this.currentScore = 0;
+  this.totalScore = 0;
+  this.playerName = "";
+}
 
 
 
